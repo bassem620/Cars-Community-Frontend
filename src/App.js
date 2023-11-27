672c924af0dd57c35293ba2bf8a3bd31505e5d98
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-11-27 18:37:41 +0200

    Pages & routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Search from './pages/Search';
+import BookAppointment from './pages/BookAppointment';
+import CarRepairs from './pages/CarRepairs';
+import Compare from './pages/Compare';
+import Events from './pages/Events';
+import EventInfo from './pages/EventInfo';
+import ItemView from './pages/ItemView';
+import News from './pages/News';
+import NewsInfo from './pages/NewsInfo';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* Nav */}
+    <Nav />
+    {/* Routes */}
+    <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />}/>
+        <Route path="/book-appointment" element={<BookAppointment />}/>
+        <Route path="/car-repair" element={<CarRepairs />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<EventInfo />} />
+        <Route path="/item/:id" element={<ItemView />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/news/:id" element={<NewsInfo />} />
+    </Routes>
+    {/* Footer */}
+    <Footer />
+</>
   );
 }
 
---
+++ src/pages/CarRepairs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CarRepairs = () => {
+    return (
+        <div>CarRepairs</div>
+    )
+}
