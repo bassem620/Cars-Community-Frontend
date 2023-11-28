6146960ba7b3fce19e4a0602259eab3c884e73b2
fatmaashraf1 <104174539+fatmaashraf1@users.noreply.github.com>
2023-11-28 03:34:53 +0200

    Events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Events = () => {
     return (
-        <div>Events</div>
+        <div className='events'>
+            <h4>Events</h4>
+            <div className="events-container">
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+                <div className='event'>
+                    <div className="events-event-inf">
+                        <h4>Sheraton Event</h4>
+                        <p>3 October 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/ItemView.jsx
+++ src/pages/ItemView.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 const ItemView = () => {
     return (
         <div className='view-item'>
-            <h4>Item Name</h4>
+            {/* <h4>Item Name</h4> */}
             <div className='item-info'>
-            <img className='view-item-img' src='https://i1.wp.com/james-henderson.com/wp-content/uploads/2022/01/Charger-006-1024x683.jpg?ssl=1'/>
+            <img className='view-item-img' src='https://c4.wallpaperflare.com/wallpaper/50/727/773/volkswagen-golf-mk7-vw-golf-7-gti-wallpaper-preview.jpg'/>
             <div className="view-item-info">
+                <h4 className='my-4'>Item Name</h4>
                 <span className=''>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quibusdam, dolorem accusamus facere consequuntur quos dolore ab quisquam magnam repudiandae saepe voluptatibus sint officiis nulla, quod doloremque provident nobis voluptates!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quia illo est optio magni reprehenderit quod vel blanditiis, quis eaque repellendus temporibus voluptatibus quisquam libero consequatur ipsum sed tempore pariatur.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia iste magnam quam nulla temporibus. Et, dolore, quo expedita inventore, pariatur placeat numquam quaerat veniam eius voluptatum architecto. Vel, delectus dolor?
                 </span>
-                <span className='view-item-price'>
+                
+                <span className='view-item-price my-3'>
                     Price : <span className='text-danger'>20$</span>
                 </span>
                 <Link to=""><button className='btn btn-dark my-1'>Compare</button></Link>
