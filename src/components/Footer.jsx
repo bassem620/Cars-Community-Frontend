1e1f55818e55d7762ebd37c18cb4a67b56be3ed5
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-11-30 10:31:01 +0200

    Components reformated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import '../index.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-0 border-top position-absolute w-100 b-0 bg-dark text-light">
-        <div class="col-md-4 d-flex align-items-center text-danger">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <svg class="bi" width="30" height="24"></svg>
-        </a>
-        <span class="mb-3 mb-md-0 text-light fs-6">vomvom ,inc</span>
-        </div>
-  </footer>
+            <div class="col-md-4 d-flex align-items-center text-danger">
+                <Link to="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <svg class="bi" width="30" height="24"></svg>
+                </Link>
+                <span class="mb-3 mb-md-0 text-light fs-6">vomvom ,inc</span>
+            </div>
+        </footer>
     )
 }
 
