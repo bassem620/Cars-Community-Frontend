e0f437b854b9b80c449813b3d1ae2a27d4df3a68
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-11-30 10:25:12 +0200

    Cars fixed

---
+++ src/pages/Cars.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function cars({title,imageurl,price}) {
+    return (
+        <div className='cars-container'>
+            <div className='image-container'>
+                <img src={imageurl} alt=''/>
+            </div>
+            <div className='card-content'>
+                <div className='car-title'>
+                    <h3>{title}</h3>
+                </div>
+                <div className='price-container'>
+                    <p>{price}</p>
+                </div>
+                <div className='btn'>
+                    <button>
+                            view info 
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default cars
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import cars from './cars'
+import Cars from './Cars'
 const Home = () => {
     return (
         <div className='home'>
-        <h4>Cars</h4>
-        <cars 
-            title="car name"
-            imageurl="https://mir-s3-cdn-cf.behance.net/projects/404/940816156947497.Y3JvcCwxMjA3LDk0NCwyMTIsODk.jpg"
-            price="price"
-        />
-       </div>
+            <h4>Cars</h4>
+            <Cars 
+                title="car name"
+                imageurl="https://mir-s3-cdn-cf.behance.net/projects/404/940816156947497.Y3JvcCwxMjA3LDk0NCwyMTIsODk.jpg"
+                price="price"
+            />
+        </div>
     )
 }
 
