cf6aa21edc2abd4fc3139156b42adea170bf0540
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-01 14:27:44 +0200

    class => className

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,33 +4,33 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <nav class="navbar navbar-expand-md navbar-dark">
-            <div class="container-fluid">
-                <Link class="navbar-brand" to="/">Cars Community</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Cars Community</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav m-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <Link class="nav-link active" aria-current="page" to="/home">Cars</Link>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav m-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/home">Cars</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" aria-current="page" to="/book-appointment">Book Appointment</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" aria-current="page" to="/book-appointment">Book Appointment</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" aria-current="page" to="/car-repair">Car Repairs</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" aria-current="page" to="/car-repair">Car Repairs</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" aria-current="page" to="/events">Events</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" aria-current="page" to="/events">Events</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" aria-current="page" to="/news">News</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" aria-current="page" to="/news">News</Link>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <Link class="badge nav-link disabled mt-2 "></Link>
-                    <Link class="btn btn-outline-success mx-2" to="/sign-in">Login </Link>
+                <form className="d-flex" role="search">
+                    <Link className="badge nav-link disabled mt-2 "></Link>
+                    <Link className="btn btn-outline-success mx-2" to="/sign-in">Login </Link>
                 </form>
                 </div>
             </div>
--- src/pages/CarBox.jsx
+++ src/pages/CarBox.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 function cars({id, title,imageurl,price}) {
     return (
-    <div class="col-12 col-sm-4 col-lg-3 p-1">
+    <div className="col-12 col-sm-4 col-lg-3 p-1">
         <div className="card w-100 h-100">
-            <img src={imageurl} class="card-img-top" alt="" />
-            <div class="card-body">
-                <h5 class="card-title">{title}</h5>
-                <p class="card-text text-dark">{price} L.E.</p>
-                <Link to={`/cars/${id}`} class="btn btn-primary">View Info</Link>
+            <img src={imageurl} className="card-img-top" alt="" />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text text-dark">{price} L.E.</p>
+                <Link to={`/cars/${id}`} className="btn btn-primary">View Info</Link>
             </div>
         </div>
     </div>
