1d94628d677ca5590dc31be5b790f5379f5f4039
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-01 13:14:13 +0200

    Cars and home edits

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function cars({title,imageurl,price}) {
+function cars({id, title,imageurl,price}) {
     return (
-        <div className='cars-container'>
-            <div className='image-container'>
-                <img src={imageurl} alt=''/>
-            </div>
-            <div className='card-content'>
-                <div className='car-title'>
-                    <h3>{title}</h3>
-                </div>
-                <div className='price-container'>
-                    <p>{price}</p>
-                </div>
-                <div className='btn'>
-                    <button>
-                          view info  
-                    </button>
-                </div>
+    <div class="col-12 col-sm-4 col-lg-3 p-1">
+        <div className="card w-100 h-100">
+            <img src={imageurl} class="card-img-top" alt="" />
+            <div class="card-body">
+                <h5 class="card-title">{title}</h5>
+                <p class="card-text text-dark">{price} L.E.</p>
+                <Link to={`/cars/${id}`} class="btn btn-primary">View Info</Link>
             </div>
         </div>
+    </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Cars from './Cars'
 const Home = () => {
     return (
-        <div className='home'>
-            <h4>Cars</h4>
-            <Cars 
-                title="toktok"
-                imageurl="https://mir-s3-cdn-cf.behance.net/projects/404/940816156947497.Y3JvcCwxMjA3LDk0NCwyMTIsODk.jpg"
-                price="100000 L.E."
-            />
+        <div className='home container-lg'>
+            <h4 className='fs-3 m-2 text-center'>Cars</h4>
+            <div className="row">
+                <Cars
+                    id={1}
+                    title="toktok"
+                    imageurl="https://mir-s3-cdn-cf.behance.net/projects/404/940816156947497.Y3JvcCwxMjA3LDk0NCwyMTIsODk.jpg"
+                    price="100000"
+                />
+            </div>
         </div>
     )
 }
