bf7c4c1148db7184f37e7217b89ae790da12939b
beshoybahaa <bahaa_beshoy@yahoo.com>
2023-12-02 02:39:21 +0200

    get date and make the counter of the date

--- src/pages/EventInfo.jsx
+++ src/pages/EventInfo.jsx
@@ -28,6 +28,36 @@ const EventInfo = () => {
 
   const [numOfLikes, setNumOfLikes] = useState("100");
   console.log(eventInfo);
+
+
+  const [dateAndTime, setDateAndTime] = useState(null);
+    useEffect(() => {
+      setDateAndTime(eventInfo.date);
+    }, [eventInfo.date]);
+    const eventDate = new Date(dateAndTime);
+    console.log(eventDate.getSeconds());
+
+    const getTimeLeft = () => {
+      const totalTimeLeft = eventDate - new Date();
+      const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
+      const seconds = Math.floor((totalTimeLeft / 1000) % 60);
+      return { days, hours, minutes, seconds };
+    };
+    
+	const [timeLeft, setTimeLeft] = useState(() => getTimeLeft());
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTimeLeft(getTimeLeft());
+		}, 1000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
   return (
     <div className="eventInfo">
       <div className="imageContainer">
@@ -38,7 +68,7 @@ const EventInfo = () => {
           <div className="d-flex justify-content-center">
             <div className="pe-2 d-flex flex-column border border-white border-start-0 border-top-0 border-bottom-0 border-4">
               <div className="ps-2">
-                <h1 className="time">00</h1>
+                <h1 className="time">{timeLeft.days}</h1>
               </div>
               <div className="p">
                 <h2 className="timeType">days</h2>
@@ -46,7 +76,7 @@ const EventInfo = () => {
             </div>
             <div className="pe-2 ps-2 d-flex flex-column border border-white border-start-0 border-top-0 border-bottom-0 border-4">
               <div className="ps-2">
-                <h1 className="time">00</h1>
+                <h1 className="time">{timeLeft.hours}</h1>
               </div>
               <div className="p">
                 <h2 className="timeType">hours</h2>
@@ -55,7 +85,7 @@ const EventInfo = () => {
 
             <div className="pe-2 ps-2 d-flex flex-column border border-white border-start-0 border-top-0 border-bottom-0 border-4">
               <div className="ps-1">
-                <h1 className="time">00</h1>
+                <h1 className="time">{timeLeft.minutes}</h1>
               </div>
               <div className="p">
                 <h2 className="timeType">min</h2>
@@ -64,7 +94,7 @@ const EventInfo = () => {
 
             <div className="pe-2 ps-2 d-flex flex-column">
               <div className="ps-1">
-                <h1 className="time">00</h1>
+                <h1 className="time">{timeLeft.seconds}</h1>
               </div>
               <div className="p">
                 <h2 className="timeType">sec</h2>
@@ -118,8 +148,7 @@ const EventInfo = () => {
                         d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"
                       />
                     </svg>
-                    Location : 
-                    {" "+eventInfo.location}
+                    Location :{" " + eventInfo.location}
                   </h2>
                 </ScrollTrigger>
               </div>
