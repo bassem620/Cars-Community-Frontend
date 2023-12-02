53ec8b3c6116fa02b5da148661338c526c80eae4
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-02 16:57:44 +0200

    events UI fixed

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -18,24 +18,22 @@ const Events = () => {
             <div className="events-container">
                 {
                     events && events.length > 0 ?
-                    <div className='event'>
-                        {
-                            events.map(event => {
-                                return (
-                                  <Link to={"/events/" + event._id}>
-                                    <div
-                                      className="events-event-inf"
-                                      key={event._id}
-                                    >
-                                      <h4>{event.title}</h4>
-                                      <span>{event.location}</span>
-                                      <p>{event.date}</p>
-                                    </div>
-                                  </Link>
-                                );
-                            })
-                        }
-                    </div>
+                        events.map(event => {
+                            return (
+                                <div className='event my-4'>
+                                    <Link to={"/events/" + event._id}>
+                                        <div
+                                        className="events-event-inf"
+                                        key={event._id}
+                                        >
+                                        <h4>{event.title}</h4>
+                                        <span>{event.location}</span>
+                                        <p>{event.date}</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            );
+                        })
                     : <h3 className='text-center'>No events found</h3>
                 }
             </div>
