061a506d4a3ce7775493924dd9dbd71420f9e7a8
beshoybahaa <bahaa_beshoy@yahoo.com>
2023-12-03 22:12:39 +0200

    css file of boba

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import "../signIn.css"
 const SignIn = () => {
-    return (   
-            <div className="grad1">
-            <h1>LogIn</h1>
-            <p> This linear gradient starts red at top left, transitioning to yellow (at bottom right):</p>
-            </div>
-    )
+    return (
+      <div className="gradient">
+        <h1>LogIn</h1>
+        <p>
+          This linear gradient starts red at top left, transitioning to yellow
+          (at bottom right):
+        </p>
+      </div>
+    );
 }
 
 export default SignIn
