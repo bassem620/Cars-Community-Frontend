e9c77153880b8c97f64498aebd25201185fd24cc
Kerolos Maher <kerolossafwat41@gmail.com>
2023-12-03 22:43:04 +0200

    Compare

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Compare = () => {
     return (
-        <div className='compare'>Compare</div>
+        <div className='compare'>
+
+            <div className="compare-part">
+                    <img src='https://images.hgmsites.net/lrg/2023-dodge-charger_100853233_l.jpg' className="compare-part-img"/>
+                    <div className="compare-part-info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem odio eaque maiores quia, ad quae, quaerat nisi pariatur quas itaque quis labore reiciendis, eos vero debitis perspiciatis omnis! Deleniti, doloremque!
+                    </div>
+            </div>
+
+            <span className='compare-title'>Compare</span>
+            
+            <div className="compare-part">
+                    <img src='https://i.insider.com/5e9a0cafdcd88c113f7c08b0?width=750&format=jpeg&auto=webp' className="compare-part-img"/>
+                    <div className="compare-part-info">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores aliquam et odit molestias molestiae, harum ullam tempore voluptas beatae unde officiis a vitae consectetur reprehenderit animi delectus, placeat perspiciatis voluptatem?
+                    </div>
+            </div>
+
+        </div>
     )
 }
 
