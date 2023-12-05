28f1f684a530820e208b3ce07d2f4e5028325b87
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-05 04:28:59 +0200

    Compare fixed

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const Compare = () => {
     const {car1,car2} = useParams();
+    const navigate = useNavigate();
     const url="https://cars-community-backend.onrender.com/cars/"+car1;
     const [cars,Setcars]=useState({});
     const [isloading,setisloading]=useState(true);
     const [carsloading,setcarsloading]=useState(true);
-    const currenturl = window.location.href;
     const [allcars, setallCars] = useState({});
     const [secCar,setsecCar] = useState({});
     const [seccarloading,setseccarloading]=useState(true);
+
+    const secCarSelection = (carId) => {
+        navigate("/compare/" + car1 + "/" + carId);
+        window.location.reload();
+    }
 
     useEffect( _ => {
         axios.get("https://cars-community-backend.onrender.com/cars/all")
@@ -35,12 +40,12 @@ const Compare = () => {
                 }).catch(function(Error){
                     console.log(Error.Response);
                 })
-        },[]
+        },[url]
     )
 
     useEffect(
         ()=>{
-            if(car2!=undefined){
+            if(car2 !== undefined){
             axios.get("https://cars-community-backend.onrender.com/cars/"+car2).then(
                 (Response) =>{
                     setsecCar(Response.data);
@@ -48,38 +53,37 @@ const Compare = () => {
                 }).catch(function(Error){
                     console.log(Error.Response);
                 })}
-        },[]
+        },[car2]
     )
-
-    
-    
 
     return (
         <div className='compare'>
             {
-                isloading==true?
+            isloading === true?
+                // Loading
                 <div className="compare-part">
                     <div className="compare-part-img"><h4>Loading...</h4></div>
                     <h5 className='my-2'>Loading...</h5>
                     <div className="compare-part-info">
-                    Loading...
+                        Loading...
                     </div>
-            </div>
+                </div>
                 :
+                // Car 1
                 <div className="compare-part">
-                    <img src={cars.data.image} className="compare-part-img"/>
+                    <img src={cars.data.image} className="compare-part-img" alt=''/>
                     <h5 className='my-2'>{cars.data.title}</h5>
                     <div className="compare-part-info">
                     {cars.data.desc}
-                    </div>
+                </div>
             </div>
             }
-
+            {/* Splitter */}
             <span className='compare-title'>Compare</span>
-            
-            {car2!=undefined?
-
-                    setseccarloading==true?
+            {/* Car 2 */}
+            {
+                car2 !== undefined?
+                    seccarloading === true?
                     <div className="compare-part">
                             <div  className="compare-part-img">Loading...</div>
                             <div className="compare-part-info">
@@ -89,40 +93,35 @@ const Compare = () => {
                     :
                     secCar.data &&
                     <div className="compare-part">
-                            <img src={secCar.data.image} className="compare-part-img"/>
+                            <img src={secCar.data.image} className="compare-part-img" alt=''/>
                             <div className="compare-part-info">
                             {secCar.data.desc}
                             </div>
                     </div>
         :
-        <div className='choose-to-compare'>
+            <div className='choose-to-compare'>
                 <h5 className='my-3'>Choose Car To Compare</h5>
-                {carsloading==true?
-                                <div className='choose-item'>
-                                <div className='choose-item-img'>Loading ...</div>
-                                <h5>Loading Car ...</h5>
-                            </div>
+                {
+                    carsloading === true?
+                        <div className='choose-item'>
+                            <div className='choose-item-img'>Loading ...</div>
+                            <h5>Loading Car ...</h5>
+                        </div>
                         :
                         allcars &&
-                            allcars.data.map(
-                                (car)=>{ return(
-                                    <Link to={currenturl+"/"+car._id} className='choose-item'>
-                                                        <img src={car.image} className='choose-item-img'/>
-                                                        <h5>{car.title}</h5>
-                                                    </Link>
+                        allcars.data.map(
+                            (car) => {
+                                return(
+                                    <Link onClick={() => secCarSelection(car._id)} className='choose-item'>
+                                        <img src={car.image} className='choose-item-img' alt=''/>
+                                        <h5>{car.title}</h5>
+                                    </Link>
                                 )
-                                                    
-                                }
-                            )
-                
-            }
-
+                            }
+                        )
+                }
             </div>
-        }
-            
-
-            
-
+            }
         </div>
     )
 }
