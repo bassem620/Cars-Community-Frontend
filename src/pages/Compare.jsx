e51d9df1bb4d69ce41a6910b2f449903aff7bf40
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-07 15:02:40 +0200

    compare fixed + price

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { baseUrl } from '../constants/constants';
 
 const Compare = () => {
     const {car1,car2} = useParams();
     const navigate = useNavigate();
-    const url="https://cars-community-backend.onrender.com/cars/"+car1;
+    const url=baseUrl + "/cars/" + car1;
     const [cars,Setcars]=useState({});
     const [isloading,setisloading]=useState(true);
     const [carsloading,setcarsloading]=useState(true);
@@ -19,7 +20,7 @@ const Compare = () => {
     }
 
     useEffect( _ => {
-        axios.get("https://cars-community-backend.onrender.com/cars/all")
+        axios.get(baseUrl + "/cars/all")
         .then(
             (Response) =>{
                 setallCars(Response.data);
@@ -46,7 +47,7 @@ const Compare = () => {
     useEffect(
         ()=>{
             if(car2 !== undefined){
-            axios.get("https://cars-community-backend.onrender.com/cars/"+car2).then(
+            axios.get(baseUrl + "/cars/" + car2).then(
                 (Response) =>{
                     setsecCar(Response.data);
                     setseccarloading(false);
@@ -62,7 +63,7 @@ const Compare = () => {
             isloading === true?
                 // Loading
                 <div className="compare-part">
-                    <div className="compare-part-img"><h4>Loading...</h4></div>
+                    <div className="compare-part-img image-fluid"><h4>Loading...</h4></div>
                     <h5 className='my-2'>Loading...</h5>
                     <div className="compare-part-info">
                         Loading...
@@ -71,8 +72,9 @@ const Compare = () => {
                 :
                 // Car 1
                 <div className="compare-part">
-                    <img src={cars.data.image} className="compare-part-img" alt=''/>
+                    <img src={cars.data.image} className="compare-part-img image-fluid" alt=''/>
                     <h5 className='my-2'>{cars.data.title}</h5>
+                    <h5 className='my-2 text-danger'>Price: {cars.data.price}</h5>
                     <div className="compare-part-info">
                     {cars.data.desc}
                 </div>
@@ -85,7 +87,7 @@ const Compare = () => {
                 car2 !== undefined?
                     seccarloading === true?
                     <div className="compare-part">
-                            <div  className="compare-part-img">Loading...</div>
+                            <div  className="compare-part-img image-fluid">Loading...</div>
                             <div className="compare-part-info">
                             Loading...
                             </div>
@@ -93,7 +95,9 @@ const Compare = () => {
                     :
                     secCar.data &&
                     <div className="compare-part">
-                            <img src={secCar.data.image} className="compare-part-img" alt=''/>
+                            <img src={secCar.data.image} className="compare-part-img image-fluid" alt=''/>
+                            <h5 className='my-2'>{secCar.data.title}</h5>
+                            <h5 className='my-2 text-danger'>Price: {secCar.data.price}</h5>
                             <div className="compare-part-info">
                             {secCar.data.desc}
                             </div>
